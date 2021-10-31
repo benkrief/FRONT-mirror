11f548ef12bdf8414e30da5d9d9a1af55517c684
Tiziano GHISOTTI <tiziano@desktop.home>
2021-10-31 11:55:14 +0100

    feat(ROAD-31): changement de branch

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import * as Material from "@angular/material";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+
+  ]
+})
+export class MaterialModule {
+}
