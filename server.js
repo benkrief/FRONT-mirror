c2fd070f7a1224abef69f78e78cd91bdbedb840d
Krief Benjamin <benjaminkri@hotmail.fr>
2021-11-03 16:37:29 +0100

    test prod

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/demopr'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/demopr/index.html'));
-});
+app.use(requireHTTPS);
+app.use(express.static('./dist/front'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4148);
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/front/'}),
+);
+
+app.listen(process.env.PORT || 8080);
