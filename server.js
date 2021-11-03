8ce085cd5f1cdee2ed43ae262f76471fd3ac0589
Krief Benjamin <benjaminkri@hotmail.fr>
2021-11-03 16:53:45 +0100

    test prod v3

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const express = require('express');
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static('./dist/front'));
+app.use(express.static(__dirname+'/dist/front'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/front/'}),
-);
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/front/index.html');
+  console.log(" Fetching from.." + fullPath);
+  res.sendFile(fullPath);
+})
 
 app.listen(process.env.PORT || 8080);
