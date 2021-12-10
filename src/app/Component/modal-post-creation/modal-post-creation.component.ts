1fab57c063303fe30bf1a5aa03853b5dcf37b5e2
Ghisotti <tiziano.ghisotti@gmail.com>
2021-12-10 15:24:56 +0100

    refactor(ROAD-~): merge

--- src/app/Component/modal-post-creation/modal-post-creation.component.ts
+++ src/app/Component/modal-post-creation/modal-post-creation.component.ts
@@ -1,55 +1,44 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, NgForm} from "@angular/forms";
+import {FormControl, FormGroup} from "@angular/forms";
 import {MatDialogRef} from "@angular/material/dialog";
 import {Post} from "../../Model/Post";
-import {catchError, map, startWith} from "rxjs/operators";
-import {of} from "rxjs";
+import {BehaviorSubject} from "rxjs";
+import {Response} from "../../Model/Response";
+import {StateEnum} from "../../Enum/state.enum";
+import {PostService} from "../../Services/Post/post.service";
 
 @Component({
   selector: 'app-post-creation',
   templateUrl: './modal-post-creation.component.html',
   styleUrls: ['./modal-post-creation.component.scss']
 })
-export class ModalPostCreationComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<ModalPostCreationComponent>) {
+export class ModalPostCreationComponent implements OnInit {
+  private dataSubject = new BehaviorSubject<Response<Post>>(null);
+
+  constructor(public dialogRef: MatDialogRef<ModalPostCreationComponent>, private postService: PostService) {
   }// service ne doit pas etre public todo: voir pourquoi ça marche pas en private n
 
   ngOnInit(): void {
   }
 
   form: FormGroup = new FormGroup({
-    $key: new FormControl(null),
-    titre: new FormControl(''),
+    title: new FormControl(''),
     description: new FormControl('')
   });
 
   initializeFormGroup() {
     this.form.setValue({
-      $key: null,
-      titre: '',
+      title: '',
       description: ''
     });
   }
 
-  createPost(postForm: NgForm): void {
-    console.log(postForm.value);
-    this.appState$ = this.postService.create$(postForm.value as Post).pipe(
-      map(response => {
-        console.log(response)
-        this.dataSubject.next(
-          {...response, data: {results: [response.data.result, ...this.dataSubject.value.data.results]}}
-        );
-        postForm.resetForm({});
-        return {state: this.stateEnum.LOADED_STATE, appData: this.dataSubject.value}
-      }),
-      startWith({state: this.stateEnum.LOADED_STATE, appData: this.dataSubject.value}),
-      catchError((error: string) => {
-        return of({state: this.stateEnum.ERROR_STATE, error})
-      })
-    )
+  createPost(postForm: FormGroup): void {
+    this.postService.create$(postForm.value as Post).subscribe(post => {
+      this.dataSubject.next(post);
+    });
   }
-
 
   postClickCancel() {
     this.dialogRef.close();
